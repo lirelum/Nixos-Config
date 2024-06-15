7bc6a844a077ac6b3d6b0e72aa24fa35269b1811
vvvvvvvvvvvvvvvv <lirelum@disroot.org>
2024-06-15 17:58:02 -0400

    added neovim
    Signed-off-by: vvvvvvvvvvvvvvvv <lirelum@disroot.org>

--- configuration.nix
+++ configuration.nix
@@ -133,13 +133,14 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     curl
     git
+    vim
+    neovim
   ];
 
-  programs.vim.defaultEditor = true;
+  programs.neovim.defaultEditor = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
