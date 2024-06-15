72d710630e4f88969b7c65f55250237e7aef5241
vvvvvvvvvvvvvvvv <lirelum@disroot.org>
2024-06-15 10:52:05 -0400

    changed default editor to vim
    Signed-off-by: vvvvvvvvvvvvvvvv <lirelum@disroot.org>

--- configuration.nix
+++ configuration.nix
@@ -139,6 +139,8 @@
     git
   ];
 
+  programs.vim.defaultEditor = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
