38c9898bde201e5262d3eeb1853c62577f364bba
vvvvvvvvvvvvvvvv <lirelum@disroot.org>
2024-06-16 12:55:34 -0400

    Changed desktop manager back to xfce
    Signed-off-by: vvvvvvvvvvvvvvvv <lirelum@disroot.org>

--- nixos/gui.nix
+++ nixos/gui.nix
@@ -3,5 +3,5 @@
 
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.windowManager.xmonad.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
 }
