4aed6ffa99f111a85d7d61d4c31b377824b7a208
vvvvvvvvvvvvvvvv <lirelum@disroot.org>
2024-06-22 20:08:28 -0400

    added new ssh key

--- nixos/users.nix
+++ nixos/users.nix
@@ -5,6 +5,7 @@
     extraGroups = ["networkmanager" "wheel"];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJSVa1FUx6NEjul53wdP51KHxyuBjShOhLGkd86jd8PP lirelum@zundamon.local"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuaNvM8eOQngd7kn3WJXDcecyafi1wl6pdCuvu0zABl lirelum@zundamon.local"
     ];
   };
 }
