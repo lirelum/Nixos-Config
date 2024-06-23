ab8bdbc8864d41992e3468a7c81f7f07fcfe34cc
vvvvvvvvvvvvvvvv <lirelum@disroot.org>
2024-06-23 12:16:57 -0400

    Changed pinetry package

--- home-manager/services/gpg.nix
+++ home-manager/services/gpg.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   services.gpg-agent = {
     enable = true;
-    pinentryPackage = pkgs.pinentry-curses;
+    pinentryPackage = pkgs.pinentry-gnome3;
     enableFishIntegration = true;
     enableBashIntegration = true;
   };
