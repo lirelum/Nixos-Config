8473fee19a2fa21d5b640bd075cbd6a5206f5887
vvvvvvvvvvvvvvvv <lirelum@disroot.org>
2024-06-23 22:03:59 -0400

    More architectures

--- flake.nix
+++ flake.nix
@@ -33,8 +33,11 @@
     inherit (self) outputs;
     # Supported systems for your flake packages, shell, etc.
     systems = [
+      "aarch64-linux"
+      "i686-linux"
       "x86_64-linux"
       "aarch64-darwin"
+      "x86_64-darwin"
     ];
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
