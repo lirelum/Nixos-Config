8df3cd33c0de1a98d8482fc30e65e8d6a3048e1e
vvvvvvvvvvvvvvvv <lirelum@disroot.org>
2024-08-06 18:45:41 -0400

    Fix gnome keyring

--- modules/nixos-common/security/default.nix
+++ modules/nixos-common/security/default.nix
@@ -1,6 +1,8 @@
 {config, ...}: {
+  security.pam.services.sddm.enableGnomeKeyring = true;
   security.pam.services.${config.local.users.default.user}.enableGnomeKeyring = true;
   services.gnome.gnome-keyring.enable = true;
+  programs.seahorse.enable = true;
   services.pcscd.enable = true;
   security.sudo = {
     execWheelOnly = true;
