1fe9c9398bcdce8372637603ddb02b81e7a50bc7
vvvvvvvvvvvvvvvv <lirelum@disroot.org>
2024-08-06 16:49:17 -0400

    Modify hyprland home config

--- modules/home-common/hyprland/default.nix
+++ modules/home-common/hyprland/default.nix
@@ -1,27 +1,30 @@
-{
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    bind =
-      [
-        "$mod, F, exec, firefox"
-        ", Print, exec, grimblast copy area"
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-        builtins.concatLists (builtins.genList (
-            x: let
-              ws = let
-                c = (x + 1) / 10;
-              in
-                builtins.toString (x + 1 - (c * 10));
-            in [
-              "$mod, ${ws}, workspace, ${toString (x + 1)}"
-              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-            ]
-          )
-          10)
-      );
+{pkgs, ...}: {
+  wayland.windowManager.hyprland = {
+    plugins = with pkgs.hyprlandPlugins; [
+      hyprbars
+    ];
+    settings = {
+      "$mod" = "SUPER";
+      bind =
+        [
+          "$mod, F, exec, firefox"
+          "$mod, Q, exit"
+          "$mod, T, exec, alacritty"
+        ]
+        ++ (
+          builtins.concatLists (builtins.genList (
+              x: let
+                ws = let
+                  c = (x + 1) / 10;
+                in
+                  builtins.toString (x + 1 - (c * 10));
+              in [
+                "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              ]
+            )
+            10)
+        );
+    };
   };
 }
-
