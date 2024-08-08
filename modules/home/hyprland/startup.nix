efcef42a25b334306d8d335d78db0e6b4c8bc82c
vvvvvvvvvvvvvvvv <lirelum@disroot.org>
2024-08-08 00:29:52 -0400

    mend

--- modules/home/hyprland/startup.nix
+++ modules/home/hyprland/startup.nix
@@ -1,6 +1,6 @@
 {pkgs,config, ...}: let
   startup = pkgs.writeShellScriptBin "startup" ''
     eval $(gnome-keyring-daemon --start --components=secrets,ssh);
-    export SSH_AUTH_SOCK = /run/user/1000/keyring/ssh"
+    export SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh"
   '';
 in {home.packages = [startup];}
